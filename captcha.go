d3385d748e9682adc9276e132cffdbf67a14a062
Erfan Momeni <Erfan.Momeni@snapp.cab>
2023-02-18 02:24:38 +0330

    feat: init project and complete logic

---
+++ captcha.go
@@ -0,0 +1,86 @@
+package captcha
+
+import (
+	"github.com/ErfanMomeniii/randstr"
+	"github.com/fogleman/gg"
+	"github.com/golang/freetype/truetype"
+	"golang.org/x/image/font/gofont/goregular"
+	"image"
+	"image/png"
+	"math/rand"
+	"os"
+	"time"
+)
+
+type Captcha struct {
+	Width      int
+	Height     int
+	FontWeight float64
+}
+
+func (c *Captcha) Numeric(length int) image.Image {
+	return draw(randstr.Dec(length), c.Width, c.Height, c.FontWeight)
+}
+
+func (c *Captcha) Alphabetical(length int) image.Image {
+	return draw(randstr.String(length, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"),
+		c.Width, c.Height, c.FontWeight)
+}
+
+func (c *Captcha) Mixed(length int) image.Image {
+	return draw(randstr.String(length), c.Width, c.Height, c.FontWeight)
+}
+
+func (c *Captcha) Save(path string, im image.Image) error {
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	return png.Encode(file, im)
+}
+
+func draw(text string, width int, height int, fontSize float64) image.Image {
+	rand.Seed(time.Now().UnixNano())
+
+	dc := gg.NewContext(width, height)
+
+	font, _ := truetype.Parse(goregular.TTF)
+
+	face := truetype.NewFace(font, &truetype.Options{
+		Size: fontSize,
+	})
+
+	dc.SetFontFace(face)
+
+	w, h := dc.MeasureString(text)
+
+	dc.DrawRoundedRectangle(0, 0, 2*w, 2*h, 20)
+
+	dc.SetHexColor("#ffffff")
+
+	dc.Fill()
+
+	dc.SetHexColor("#8AAAE5")
+
+	u := 1
+	if rand.Intn(2)%2 == 0 {
+		u = -1
+	}
+
+	dc.RotateAbout(gg.Radians(float64(u*7)), w/2, h/2)
+
+	dc.DrawString(text, w/2, h+h/2)
+
+	dc.Stroke()
+
+	return dc.Image()
+}
+
+func New(width int, height int, fontWeight float64) *Captcha {
+	return &Captcha{
+		Width:      width,
+		Height:     height,
+		FontWeight: fontWeight,
+	}
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/ErfanMomeniii/captcha.git
+
+go 1.19
+
+require (
+	github.com/ErfanMomeniii/randstr v0.0.0-20230217223820-7a163e2ab1a4
+	github.com/fogleman/gg v1.3.0
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
+	golang.org/x/image v0.5.0
+)
